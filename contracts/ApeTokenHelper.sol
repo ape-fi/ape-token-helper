62ffc6d083f7badc05ae8ff1a85fe743ad57a26d
devapefi <dev@ape.finance>
2022-05-15 13:08:38 +0800

    contracts/ApeTokenHelper.sol: Rename contract

--- contracts/ApeTokenHelper.sol
+++ contracts/ApeTokenHelper.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./interfaces/ApeTokenInterface.sol";
 import "./interfaces/ComptrollerInterface.sol";
 
-contract CTokenHelper is Ownable {
+contract ApeTokenHelper is Ownable {
     using SafeERC20 for IERC20;
 
     ComptrollerInterface public immutable comptroller;
